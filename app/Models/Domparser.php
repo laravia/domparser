dd8f3f73396b0f27d76d1e09df1a5ade0b2a0fd4
manuel <code@leunam.art>
2023-11-21 07:42:27 +0100

    [feat] added filter and searchkey + factories

--- app/Models/Domparser.php
+++ app/Models/Domparser.php
@@ -2,18 +2,26 @@
 
 namespace Laravia\Domparser\App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravia\Domparser\Database\Factories\DomparserFactory;
 use Laravia\Heart\App\Models\Model;
 use Orchid\Screen\AsSource;
 
 class Domparser extends Model
 {
-    use AsSource;
+    use AsSource, HasFactory;
     protected $fillable = [
         'url',
-        'element',
+        'filter',
+        'searchkey',
         'cronjob',
         'email',
         'unique',
     ];
+
+    protected static function newFactory()
+    {
+        return DomparserFactory::new();
+    }
 
 }
