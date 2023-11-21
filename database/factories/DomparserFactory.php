cc1dfda77e549715fc0ef852aa186c1325294cab
manuel <code@leunam.art>
2023-11-21 07:42:50 +0100

    [feat] added factories

---
+++ database/factories/DomparserFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Laravia\Domparser\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Laravia\Domparser\App\Models\Domparser;
+
+class DomparserFactory extends Factory
+{
+    protected $model = Domparser::class;
+
+    public function definition()
+    {
+        return [
+            'url' => $this->faker->url,
+            'filter' => $this->faker->word,
+            'searchkey' => 'test',
+            'cronjob' => "* * * * *",
+            'email' => $this->faker->email,
+            'unique' => true
+        ];
+    }
+}
